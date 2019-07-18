6ae0b74729c7c55d1dcfd16d4791b10860c118fe
xialingfu <871414663@qq.com>
2019-07-18 20:05:54 +0800

    跨域问题

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        jmluang\weapp\WeappLoginServiceProvider::class
+        jmluang\weapp\WeappLoginServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
     ],
 
