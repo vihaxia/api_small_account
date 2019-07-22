cab69e9a31a3cc2cc21a6524ea02698927a4b667
xialingfu <871414663@qq.com>
2019-07-22 10:47:00 +0800

    用户管理接口

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,19 +6,24 @@ use App\Model\User;
 
 class UserController extends Controller
 {
-    public function index()
-    {
+    /**
+     * 用户管理
+     *
+     * @author xialingfu
+     * @return false|string
+     */
+    public function index() {
         $user = User::find($this->userId);
         if ($user) {
-            return [
-                'code' => 888,
+            return json_encode([
+                'code' => 0,
                 'data' => $user['user_info']
-            ];
+            ]);
         }
 
-        return [
+        return json_encode([
             'code' => -1,
             'error' => 'skey错误'
-        ];
+        ]);
     }
 }
