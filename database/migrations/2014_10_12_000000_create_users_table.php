077b129926139fd53edc09146de56d01115bdf5a
xialingfu <871414663@qq.com>
2019-07-24 04:52:22 +0800

    重新维护users表

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,17 +14,26 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
+            $table->string('phone')->nullable()->unique();
             $table->string('weapp_openid')->nullable()->comment('微信开放id');
-            $table->string('weapp_session_key')->nullable()->comment('微信session_key');
-            $table->string('nickname')->nullable()->comment('昵称');
             $table->string('weapp_avatar')->nullable()->comment('微信头像');
+            $table->string('nickname')->nullable()->comment('昵称');
+            $table->string('weapp_session_key')->nullable()->comment('微信session_key');
             $table->string('country')->nullable()->comment('国家');
             $table->string('province')->nullable()->comment('省份');
             $table->string('city')->nullable()->comment('所在城市');
             $table->string('language')->nullable()->comment('语言');
             $table->json('location')->nullable()->comment('当前地理信息');
             $table->enum('gender', ['1', '2'])->default('1')->comment('性别默认男');
-            $table->string('phone')->nullable()->unique();
+            $table->integer('level')->default('1')->comment('用户等级 1 2 3 4');
+            $table->integer('is_admin')->default('0')->comment('0 否 1 是');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
