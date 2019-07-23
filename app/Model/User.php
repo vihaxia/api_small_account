9db29161d076c7c68b5f9f338aec5d5f5c339182
xialingfu <871414663@qq.com>
2019-07-24 05:09:02 +0800

    fix

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,8 +3,31 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
 
 class User extends Model
 {
-    protected $table = 'weapp_user';
+    use HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'email',
+        'email_verified_at',
+        'username',
+        'phone',
+        'weapp_openid',
+        'nickname',
+        'weapp_avatar',
+        'country',
+        'province',
+        'city',
+        'language',
+        'location',
+        'gender',
+        'level',//用户等级
+        'is_admin',//is管理员
+    ];
 }
