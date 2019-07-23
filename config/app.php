eda7d1e7ca497f71f89cac4809e5ec9077ddab1e
xialingfu <871414663@qq.com>
2019-07-24 04:21:19 +0800

    删除webapp包

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        jmluang\weapp\WeappLoginServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
 
     ],
@@ -227,7 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'WeappUserRepository' => jmluang\weapp\Facades\WeappUser::class
     ],
 
 ];
