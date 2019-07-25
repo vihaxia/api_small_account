98d8561ccd9e91a5436cbbb7724d9fff1866ff5f
xialingfu <871414663@qq.com>
2019-07-25 23:34:39 +0800

    调测首页接口

--- app/Http/Controllers/Api/V1/Controller.php
+++ app/Http/Controllers/Api/V1/Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Model\User;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -11,4 +13,11 @@ use Dingo\Api\Routing\Helpers;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Helpers;
+
+    protected $userId;
+
+    public function __construct(Request $request)
+    {
+        $this->userId = User::where('weixin_session_key', $request->header('X-WX-Skey'))->find()['id'] ?? 0;
+    }
 }
